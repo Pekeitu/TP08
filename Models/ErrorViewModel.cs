c5595a0e394fb610711810879ad14346cddb04ef
Pekeitu <106318905+Pekeitu@users.noreply.github.com>
2022-09-13 09:03:09 -0300

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP08_Finkel_Rozen.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
